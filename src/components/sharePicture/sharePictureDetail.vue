2294ee8104d686a12ecaafbecebb371209197759
baaaaazinga <1403512022@qq.com>
2019-07-12 02:38:02 +0800

    修复图片分类导航无法滑动的bug

--- src/components/sharePicture/sharePictureDetail.vue
+++ src/components/sharePicture/sharePictureDetail.vue
@@ -2,22 +2,10 @@
   <div class="share-picture">
     <div id="slider" class="mui-slider">
       <div id="sliderSegmentedControl" class="mui-scroll-wrapper mui-slider-indicator 
-              mui-segmented-control mui-segmented-control-inverted" data-scroll="1">
-        <div class="mui-scroll" style="transform: translate3d(0px, 0px, 0px) translateZ(0px);">
-          <a class="mui-control-item mui-active" href="#item1mobile" data-wid="tab-top-subpage-1.html">
-            推荐
-          </a>
-          <a class="mui-control-item" href="#item2mobile" data-wid="tab-top-subpage-2.html">
-            热点
-          </a>
-          <a class="mui-control-item" href="#item3mobile" data-wid="tab-top-subpage-3.html">
-            北京
-          </a>
-          <a class="mui-control-item" href="#item4mobile" data-wid="tab-top-subpage-4.html">
-            社会
-          </a>
-          <a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
-            娱乐
+              mui-segmented-control mui-segmented-control-inverted">
+        <div class="mui-scroll">
+          <a :class="['mui-control-item', item.id == 0 ? 'mui-active':'']" v-for="item in cates" :key="item.id">
+            {{ item.title }}
           </a>
         </div>
       </div>
@@ -26,22 +14,40 @@
 </template>
 
 <script>
-// import mui from '../../lib/mui/js/mui.js'
 import mui from '../../../node_modules/mint-ui/lib/mui.js'
-
 export default {
+  data() {
+    return {
+      cates: [],
+    }
+  },
+  created() {
+    this.getAllCategory();
+  },
+
   mounted() {
+    // console.log(mui)
     mui('.mui-scroll-wrapper').scroll({
-      deceleration: 0.0002 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+      deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
     });
+  },
+
+  methods: {
+    getAllCategory(){
+      this.$axios.get("http://www.liulongbin.top:3005/api/getimgcategory").then((result) => {
+        // console.log(result.data.message)
+        if(result.data.status === 0){
+          result.data.message.unshift({ title:"全部", id: 0 });
+          this.cates = result.data.message;
+        }
+      })
+    }
   }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
   *{
     touch-action: pan-y;
   }
-
 </style>
